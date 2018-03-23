d3a18c2fa9513c5653b2eb0188cf3242c4e3da4c
Stephen Smith <stephen@mapsmith.net>
2018-03-22 23:25:00 -0400

    much simpler with mapshaper

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-var fs = require('fs')
-var shapefile = require("shapefile");
-var gj2svg = require('geojson-to-svg'); // factory 
+var mapshaper = require('mapshaper')
 
-global.geojson = {};
-global.geojson['type'] = 'FeatureCollection';
-global.geojson['features'] = [];
-
-shapefile.open("QPF168hr_Day1-7_latest/97e2200.shp")
-  .then(source => source.read()
-    .then(function log(result) {
-      if (result.done) return;
-
-      var filename = '97e2200.geojson'
-      geojson.features.push(result.value)
-      fs.writeFile(filename, JSON.stringify(geojson), 'utf8');
-
-      return source.read().then(log);
-    }))
-  .catch(error => console.error(error.stack));
+mapshaper.runCommands('-i _data/QPF168hr_Day1-7_latest/97e2200.shp -proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs -o _out/ format=svg', function (results, err) {console.log(results);});
