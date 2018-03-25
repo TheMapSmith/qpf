429f43658904dd3ff117b464dd5b14a639c74052
Stephen Smith <stephen@mapsmith.net>
2018-03-25 18:16:28 -0400

    try to add svg-style where clause

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var mapshaper = require('mapshaper')
 var i = '-i 97e2200.shp '
 var proj = '-proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs '
 var svgstyle = '-svg-style fill=#303545 opacity=0.25 '
+var qpf1 = "-svg-style where=QPF>1 fill=red "
 var o = '-o format=svg '
 
-var command = i + proj + svgstyle + o
+var command = i + proj + svgstyle + qpf1 + o
 
 var inputs = {}
 inputs['97e2200.shp'] = '_data/QPF168hr_Day1-7_latest/97e2200.shp'
