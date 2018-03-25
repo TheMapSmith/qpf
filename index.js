c6c52bf7acf2084018a30cc7d454481141a1bc48
Stephen Smith <stephen@mapsmith.net>
2018-03-24 23:48:33 -0400

    add -svg-style and break up command

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var fs = require('fs')
 var mapshaper = require('mapshaper')
 
-// mapshaper.runCommands('-i _data/QPF168hr_Day1-7_latest/97e2200.shp -proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs -o _out/ format=svg', function (results, err) {console.log(results);});
+var i = '-i 97e2200.shp '
+var proj = '-proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs '
+var svgstyle = '-svg-style fill=#303545 opacity=0.25 '
+var o = '-o format=svg '
 
-var command = '-i _data/QPF168hr_Day1-7_latest/97e2200.shp -proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs -o _out/ format=svg'
+var command = i + proj + svgstyle + o
 
 var inputs = {}
-inputs.filename = '_data/QPF168hr_Day1-7_latest/97e2200.shp'
+inputs['97e2200.shp'] = '_data/QPF168hr_Day1-7_latest/97e2200.shp'
 
 mapshaper.applyCommands(command, inputs, function done (err, out){
   if (err) {
     console.log(err);
   }
   else {
-    fs.writeFile('97e2200.svg', out['_out/97e2200.svg'], 'utf8')
+    fs.writeFile('_out/97e2200.svg', out['97e2200.svg'], 'utf8')
   }
 })
