febd6221f22ea7ae1bebb421095f5b6fbc42b885
Stephen Smith <stephen@mapsmith.net>
2018-03-26 18:50:40 -0400

    add jsftp get

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-var mapshaper = require('mapshaper')
+const mapshaper = require('mapshaper')
+const fs = require('fs');
 
-var commandSlugs = []
+const jsftp = require("jsftp");
 
-commandSlugs.push('-i _data/QPF168hr_Day1-7_latest/97e2200.shp')
-commandSlugs.push('-proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
-// commandSlugs.push('-colorizer name=precip')
-// commandSlugs.push('colors=#21313D,#304D5D,#3C6C7E,#468C9E,#4EAFBE,#55D3DC,#5EF8F9,white')
-// commandSlugs.push('breaks=.1,.25,.75,1.5,2.5,3.5,4')
-// commandSlugs.push('-svg-style name=precip')
-commandSlugs.push('-svg-style where=QPF<20 fill=#20313E')
-commandSlugs.push('-svg-style where=QPF<10 fill=#2D4958')
-commandSlugs.push('-svg-style where=QPF<5 fill=#386374')
-commandSlugs.push('-svg-style where=QPF<3 fill=#417E90')
-commandSlugs.push('-svg-style where=QPF<2 fill=#499BAC')
-commandSlugs.push('-svg-style where=QPF<1.5 fill=#50B9C7')
-commandSlugs.push('-svg-style where=QPF<1 fill=#57D8E0')
-commandSlugs.push('-svg-style where=QPF<.25 fill=#5EF8F9')
-commandSlugs.push('-svg-style where=QPF<.1 fill=#CEFDFD') //prev color @ 90 lighness vs 67
-commandSlugs.push('-o _out/ format=svg')
+const ftp = new jsftp({
+  host: 'ftp.wpc.ncep.noaa.gov',
+  user: 'anonymous',
+  password: 'user@example.com'
+});
 
-var command = commandSlugs.join(' ')
+fetchQPF()
 
-mapshaper.runCommands(command, function(err){
+function fetchQPF() {
+  ftp.get('/shapefiles/qpf/7day/QPF168hr_Day1-7_latest.tar', 'qpf.tar', err => {
   if (err) {
     console.log(err);
   }
   else {
-    console.log("Success");
+    console.log("File copied successfully!");
   }
-})
+});
+}
 
+function processMap(qpf) {
+  var commandSlugs = []
 
-'#20313E,#2D4958,#386374,#417E90,#499BAC,#50B9C7,#57D8E0,#5EF8F9'
+  commandSlugs.push('-i ${qpf}')
+  commandSlugs.push('-proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
+  // commandSlugs.push('-colorizer name=precip')
+  // commandSlugs.push('colors=#21313D,#304D5D,#3C6C7E,#468C9E,#4EAFBE,#55D3DC,#5EF8F9,white')
+  // commandSlugs.push('breaks=.1,.25,.75,1.5,2.5,3.5,4')
+  // commandSlugs.push('-svg-style name=precip')
+  commandSlugs.push('-svg-style where=QPF<20 fill=#20313E')
+  commandSlugs.push('-svg-style where=QPF<10 fill=#2D4958')
+  commandSlugs.push('-svg-style where=QPF<5 fill=#386374')
+  commandSlugs.push('-svg-style where=QPF<3 fill=#417E90')
+  commandSlugs.push('-svg-style where=QPF<2 fill=#499BAC')
+  commandSlugs.push('-svg-style where=QPF<1.5 fill=#50B9C7')
+  commandSlugs.push('-svg-style where=QPF<1 fill=#57D8E0')
+  commandSlugs.push('-svg-style where=QPF<.25 fill=#5EF8F9')
+  commandSlugs.push('-svg-style where=QPF<.1 fill=#CEFDFD') //prev color @ 90 lighness vs 67
+  commandSlugs.push('-o _out/ format=svg')
+
+  var command = commandSlugs.join(' ')
+
+  mapshaper.runCommands(command, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success");
+    }
+  })
+
+}
