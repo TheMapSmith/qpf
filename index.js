acdc56dcd55bdcea1c6be84b72aea1f5b4ee7c97
Stephen Smith <stephen@mapsmith.net>
2018-03-26 10:23:25 -0400

    change mapshaper method back to runCommand and add initial SVG styling

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-var fs = require('fs')
 var mapshaper = require('mapshaper')
 
-var i = '-i 97e2200.shp '
-var proj = '-proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs '
-var svgstyle = '-svg-style fill=#303545 opacity=0.25 '
-var qpf1 = "-svg-style where=QPF>1 fill=red "
-var o = '-o format=svg '
+var commandSlugs = []
 
-var command = i + proj + svgstyle + qpf1 + o
+commandSlugs.push('-i _data/QPF168hr_Day1-7_latest/97e2200.shp')
+commandSlugs.push('-proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
+commandSlugs.push('-svg-style where=QPF>4 fill=#21313D')
+commandSlugs.push('-svg-style where=QPF<4 fill=#304D5D')
+commandSlugs.push('-svg-style where=QPF<3.5 fill=#3C6C7E')
+commandSlugs.push('-svg-style where=QPF<2.5 fill=#468C9E')
+commandSlugs.push('-svg-style where=QPF<1.5 fill=#4EAFBE')
+commandSlugs.push('-svg-style where=QPF<.75 fill=#55D3DC')
+commandSlugs.push('-svg-style where=QPF<.25 fill=#5EF8F9')
+commandSlugs.push('-svg-style where=QPF<.1 fill=white')
+commandSlugs.push('-o _out/ format=svg')
 
-var inputs = {}
-inputs['97e2200.shp'] = '_data/QPF168hr_Day1-7_latest/97e2200.shp'
+var command = commandSlugs.join(' ')
 
-mapshaper.applyCommands(command, inputs, function done (err, out){
+mapshaper.runCommands(command, function(err){
   if (err) {
     console.log(err);
   }
   else {
-    fs.writeFile('_out/97e2200.svg', out['97e2200.svg'], 'utf8')
+    console.log("Success");
   }
 })
+
+
+'#21313D,#304D5D,#3C6C7E,#468C9E,#4EAFBE,#55D3DC,#5EF8F9'
