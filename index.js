1129cd3a6a0b92eed85838812b471e4db7d21ab0
Stephen Smith <stephen@mapsmith.net>
2018-03-26 22:21:40 -0400

    add check for local files to make sure only necessary files are fetched

--- index.js
+++ index.js
@@ -3,22 +3,67 @@ const fs = require('fs');
 const tar = require('tar-fs')
 const jsftp = require("jsftp");
 
-const ftp = new jsftp({
-  host: 'ftp.wpc.ncep.noaa.gov',
-  user: 'anonymous',
-  password: 'user@example.com'
-});
+checkLocalFiles()
 
-getFilenames()
+// we need to know what tars have been downloaded so that when we look at the
+// filelist returned in getFilenames() we only download it if we need to
+function checkLocalFiles() {
+  fs.readdir('_data/', function(err,results){
+    if (!err) {
+      // loop through the filenames and find any instance of tar
+      var tars = []
+      results.forEach(function(e) {
+        if (e.indexOf('tar') > 0) {
+          tars.push(e)
+        }
+      })
 
-function getFilenames () {
+      // separate out the date portion of the filename and sort to get the newest
+      var tardates = []
+      tars.forEach(function(e) {
+        tardates.push(e.substr(4,10))
+      })
+      tardates.sort()
+
+      // get the last date, except if it's the only date
+      if (tardates.length > 1) {
+        var lastTarDate = tardates.pop()
+      }
+      else {
+        lastTarDate = tardates[0]
+      }
+
+      // rebuild the filename
+      var latestTar = '97e_' + lastTarDate + '.tar'
+
+      getFilenames(latestTar)
+    }
+    else {
+      console.log(err);
+    }
+  })
+}
+
+function getFilenames (latestTar) {
+
+  const ftp = new jsftp({
+    host: 'ftp.wpc.ncep.noaa.gov',
+    user: 'anonymous',
+    password: 'user@example.com'
+  });
+
     ftp.ls('/shapefiles/qpf/7day/', (err,res) => {
       if (err) {
         console.log(err);
       }
       else {
         var last = res.slice(-2)
-        fetchQPF(last[0].name)
+        if (last[0].name == latestTar) {
+          shpFilename(latestTar)
+        }
+        else {
+          fetchQPF(last[0].name)
+        }
       }
     })
 }
@@ -58,15 +103,15 @@ function shpFilename (qpftar){
 }
 
 
-function processMap(qpf) {
+function processMap(qpfshp) {
   var commandSlugs = []
 
-  commandSlugs.push(`-i ${qpf}`)
+  commandSlugs.push(`-i ${qpfshp}`)
   commandSlugs.push('-proj +proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
   // commandSlugs.push('-colorizer name=precip')
   // commandSlugs.push('colors=#21313D,#304D5D,#3C6C7E,#468C9E,#4EAFBE,#55D3DC,#5EF8F9,white')
   // commandSlugs.push('breaks=.1,.25,.75,1.5,2.5,3.5,4')
-  // commandSlugs.push('-svg-style name=precip')
+  // commandSlugs.push('-svg-style fill-color=precip(QPF)')
   commandSlugs.push('-svg-style where=QPF<20 fill=#20313E')
   commandSlugs.push('-svg-style where=QPF<10 fill=#2D4958')
   commandSlugs.push('-svg-style where=QPF<5 fill=#386374')
